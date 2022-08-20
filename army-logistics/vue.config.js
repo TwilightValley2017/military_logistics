6f57165ededbed5ff0b13e99bc951e6258fedd74
TwilightValley2017 <tenderyoung@yeah.net>
2022-08-20 09:55:37 +0800

    feat: config file name of webpack built

--- army-logistics/vue.config.js
+++ army-logistics/vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    output: {
+      hashFunction: 'xxhash64',
+      path: 'D:\\Repository\\github\\military_logistics\\army-logistics\\dist',
+      filename: 'js/army.[contenthash:8].js',
+      publicPath: '/',
+      chunkFilename: 'js/army.[contenthash:8].js'
+    },
+  }
 })
